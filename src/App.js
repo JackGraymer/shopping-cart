61b65fecc5bce6a74207d2c47785c7091ca12f03
Jack Graymer <jackgraymer@gmail.com>
2023-02-08 01:23:14 +0100

    Installs Router, create routes in Nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom';
 
 function App() {
-  
+  return(
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+const Header = () => {
+  return(
+    <div className='nav'> 
+      <h2><NavLink to=''>Shop Name</NavLink></h2>
+      <div className='nav-links'>
+        <NavLink to=''>Home</NavLink>
+        <NavLink to=''>Products</NavLink>
+        <NavLink to=''>Contact</NavLink>
+      </div>
+    </div>
+  )
+}
+
+let jsonData = function() {
+  fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then(console.log)
+
+}
+
+
+
+
+
+export default App
+
