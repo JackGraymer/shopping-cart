872e529c1960eaafdcee3817a085f8ea175dde0f
Jack Graymer <jackgraymer@gmail.com>
2023-02-09 07:36:47 +0100

    Removes fake links

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import './components.css'
+import background from "./background.jpeg"
 
 const Contact = () => {
     return(
@@ -8,8 +9,7 @@ const Contact = () => {
                     <h1> Where Technology Becomes Houseware</h1>
                     <h2> Get the gadgets tha you need in one place</h2>
                     <h3> This company has a simple goal, to make all your tech needs available. <br/>
-                    <a href="https://www.w3schools.com/">hello</a>
-                    <Link to='https://www.w3schools.com/'> jiya</Link>
+                    
                     <hr></hr></h3>
                 </div>
                 
